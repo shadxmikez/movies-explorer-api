2f25df2889fa712023181057a008df135bc4842d
Dave Ivashevic <shadxmikez@yndex.ru>
2023-12-11 22:18:39 +0300

    level-1

--- utils/setup.js
+++ utils/setup.js
@@ -2,7 +2,7 @@ const {
   PORT = 3000,
   DB = 'mongodb://localhost:27017/diplomdb',
   NODE_ENV,
-  JWT_SECRET = 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcwMTg5NzgyMSwiaWF0IjoxNzAxODk3ODIxfQ.0KvAe2UnejLXDTFBAg_dwxygFwiKWPwXf0WB91Y35FI',
+  JWT_SECRET,
 } = process.env;
 
 module.exports = {
